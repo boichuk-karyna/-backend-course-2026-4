c77cd03a6973444c2829be7877730da2d584e15e
Karyna bc2026-4 <Karyna.Boichuk@lnu.edu.ua>
2026-04-04 12:19:37 +0300

    Add commander usage

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello backend course");
+const { Command } = require("commander");
+
+const program = new Command();
+
+program
+  .option("-n, --name <type>", "your name")
+  .option("-a, --age <number>", "your age");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+console.log("Options:", options);
